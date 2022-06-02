28ca89a8514be06ed7642dcbc3c4b5ba2402234f
sudheerhadoop <sudhe9630@gmail.com>
2022-06-02 16:25:32 +0530

    Create 23. Max Sum in the Configuration.java

---
+++ 23. Max Sum in the Configuration.java
@@ -0,0 +1,36 @@
+
+/** we need t0 remember this formula **/ 
+//currentValue += sumElements - n*A[n-i]
+
+class GfG
+{
+public static int max_sum(int A[], int n)
+    {
+	// Your code here
+    	
+    int currentValue = 0; 
+    int maxProductSum = 0;
+    int sumElements = 0;
+    
+    for(int i=0;i<n;i++)
+    {
+        sumElements += A[i];
+        currentValue += i*A[i];
+    }
+    
+    maxProductSum = currentValue;
+    
+       for (int i = 1; i < n; i++)
+        {
+            currentValue += sumElements - n*A[n-i];
+        
+        if(currentValue > maxProductSum)
+        {
+            maxProductSum = currentValue;
+        }
+    }
+    
+    return maxProductSum;
+}
+
+}
