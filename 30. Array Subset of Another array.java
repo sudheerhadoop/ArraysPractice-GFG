01e9a949aa2275c1c817a3d370293cba8837f9bb
sudheerhadoop <sudhe9630@gmail.com>
2022-06-04 15:04:39 +0530

    Update 30. Array Subset of Another array.java

--- 30. Array Subset of Another array.java
+++ 30. Array Subset of Another array.java
@@ -23,3 +23,42 @@ class Compute {
         
     }
 }
+
+/**** Using Two pointers *****/
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+     Arrays.sort(a1);
+     Arrays.sort(a2);
+     
+     int i=0;
+     int j=0;
+     
+     while(i<n && j < m)
+     {
+         if(a1[i] < a2[j])
+         {
+             i++;
+             
+         }
+         else if(a1[i] == a2[j])
+         {
+             i++;
+             j++;
+         }
+         else if(a1[i] > a2[j])
+         {
+             return "No";
+         }
+     }
+     
+     if(j<m)
+     {
+         return "No";
+     }
+     else
+     {
+         return "Yes";
+     }
+    }  
+}
